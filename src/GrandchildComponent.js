b2800d031fc671863bdcf41252fa112272cc71f8
Tami Williams <tami@asktami.com>
2019-09-15 06:39:27 -0400

    changed increment

--- src/GrandchildComponent.js
+++ src/GrandchildComponent.js
@@ -9,8 +9,8 @@ const GrandchildComponent = props => {
 			{context => (
 				<div>
 					<h2>GrandhildComponent</h2>
-					<button onClick={() => context.setCount(context.count + 1)}>
-						Count + 1
+					<button onClick={() => context.setCount(context.count + 5)}>
+						Count + 5
 					</button>{' '}
 					See Count in GrandchildComponent ({context.count})
 				</div>
